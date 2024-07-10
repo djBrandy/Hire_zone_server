09222ba2ec06e994076df56282465806025d0a5f
kamusarihanshi <ahmedizaka9@gmail.com>
2024-07-10 13:14:51 +0300

    jobseekers

--- jobseekers.py
+++ jobseekers.py
@@ -5,5 +5,53 @@ from models import db
 jobseeker_bp = Blueprint('employer_bp',__name__,url_prefix='/jobseekers')
 api_bp=Api(jobseeker_bp)
 jobseeker_parser = reqparse.RequestParser()
-jobseeker_parser.add_argument('name', type=str, required=True, help='name is required')
+jobseeker_parser.add_argument('first_name', type=str, required=True, help='name is required')
+jobseeker_parser.add_argument('middle_name', type=str)
+jobseeker_parser.add_argument('last_name', type=str, required=True, help='last_name is required')
 jobseeker_parser.add_argument('email', type=str, required=True, help='email is required')
+jobseeker_parser.add_argument('phone_number', type=int, required=True, help='phone_number is required')
+
+
+class JobSeekers(Resource):
+    def get(self,id):
+        jobseeker = JobSeekers.query.get_or_404(id)
+        return {'id': jobseeker.id,'first_name':jobseeker.first_name,"middle_name":jobseeker.middle_name,"last_name":jobseeker.last_name,"email":jobseeker.email,"phone_number":jobseeker.phone_number}
+    
+    def put(self,id):
+        data = jobseeker_parser.parse_args()
+        jobseeker = JobSeekers.query.get_or_404(id)
+        jobseeker.first_name = data['first_name']
+        jobseeker.middle_name = data['middle_name']
+        jobseeker.last_name = data['last_name']
+        jobseeker.email = data['email']
+        jobseeker.phone_number = data['phone_number']
+        db.session.commit()
+        return {'id': jobseeker.id,'first_name':jobseeker.first_name,"middle_name":jobseeker.middle_name,"last_name":jobseeker.last_name,"email":jobseeker.email,"phone_number":jobseeker.phone_number}
+    
+    def delete(self,id):
+        jobseeker = JobSeekers.query.get_or_404(id)
+        db.session.delete(jobseeker)
+        db.session.commit()
+        return {'message': 'Job Seeker deleted successfully'}
+    
+
+api_bp.add_resource(JobSeekers, '/<int:id>')
+
+
+class JobSeekersList(Resource):
+    def get(self):
+        jobseekers = JobSeekers.query.all()
+        return [{'id':jobseeker.id,'first_name':jobseeker.first_name,"middle_name":jobseeker.middle_name,"last_name":jobseeker.last_name,"email":jobseeker.email,"phone_number":jobseeker.phone_number} for jobseeker in jobseekers]
+    
+    def post(self):
+        data = jobseeker_parser.parse_args()
+        new_jobseeker = JobSeekers(first_name=data['first_name'], middle_name=data['middle_name'], last_name=data['last_name'], email=data['email'], phone_number=data['phone_number'])
+        db.session.add(new_jobseeker)
+        db.session.commit()
+        return {'message': 'Job Seeker successfully created'}
+    
+    def delete(self,id):
+        jobseeker = JobSeekers.query.get(id)
+        db.session.delete(jobseeker)
+        db.session.commit()
+        return {'message': 'Job Seeker deleted successfully'}
